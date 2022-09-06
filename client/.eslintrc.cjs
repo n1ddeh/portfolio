93522daf8cfbb72bce47fd1003d3a7c5c11ded15
Mark Minkoff <mark@managexr.com>
2022-09-05 19:40:27 -0700

    eslint configuration

---
+++ client/.eslintrc.cjs
@@ -0,0 +1,46 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: [
+        'standard-with-typescript',
+        'prettier',
+        'prettier/prettier',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:react/recommended',
+        'plugin:prettier/recommended',
+    ],
+    parser: '@typescript-eslint/parser',
+    overrides: [
+        {
+            files: ['*.ts', '*.tsx'],
+            extends: [
+                'plugin:@typescript-eslint/recommended',
+                'plugin:@typescript-eslint/recommended-requiring-type-checking',
+            ],
+            parserOptions: {
+                project: ['tsconfig.json'],
+            },
+        },
+    ],
+    parserOptions: {
+        project: 'tsconfig.json',
+        tsconfigRootDir: __dirname,
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react', '@typescript-eslint', 'prettier'],
+    rules: {
+        'react/react-in-jsx-scope': 'off',
+        camelcase: 'error',
+        'spaced-comment': 'error',
+        quotes: ['error', 'single'],
+        'no-duplicate-imports': 'error',
+    },
+    settings: {
+        'import/resolver': {
+            typescript: {},
+        },
+    },
+}
