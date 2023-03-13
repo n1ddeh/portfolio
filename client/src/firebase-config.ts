150ae19af5cdda309dc55b13ae09a6bd2b7da133
n1ddeh <markminkoff00@gmail.com>
2023-03-13 00:40:13 -0700

    more

---
+++ client/src/firebase-config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+    apiKey: 'AIzaSyD6at1TTBkNg2ikAkbba68nqhcNo4qg5n4',
+    authDomain: 'portfolio-77ebd.firebaseapp.com',
+    projectId: 'portfolio-77ebd',
+    storageBucket: 'portfolio-77ebd.appspot.com',
+    messagingSenderId: '899265676661',
+    appId: '1:899265676661:web:57fe5df5bd654e3318ff3c',
+}
+
+initializeApp(firebaseConfig)
